3e93e2c25f0abeaf3a7a06b60f382aa2533b77fd
oxavelar <omar.avelar@gmail.com>
2013-05-26 20:35:52 -0500

    Our prebuilt Kernel has smartassV2.

--- recovery/postrecoveryboot.sh
+++ recovery/postrecoveryboot.sh
@@ -6,9 +6,5 @@
 # The following are some performance and brightness settings that we can overr-
 # -ide in order to save power during recovery and let the phone get less hot
 echo 40 > /sys/class/backlight/psb-bl/brightness
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo "95" > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-echo "2000000" > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-echo "120000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-echo "60000" > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-echo "90000" > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+echo "smartassV2" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
